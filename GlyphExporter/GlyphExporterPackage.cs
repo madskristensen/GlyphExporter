0fcfaaf2a762ecfd4e0dbcedb9c80954f2a22af9
Mads Kristensen <post@madskristensen.net>
2015-01-20 13:35:20 -0800

    Initial commit

---
+++ GlyphExporter/GlyphExporterPackage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Shell;
+
+namespace MadsKristensen.GlyphExporter
+{
+
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(GuidList.guidGlyphExporterPkgString)]
+    public sealed class GlyphExporterPackage : Package
+    {
+        internal IGlyphService _glyphService { get; set; }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            IComponentModel model = (IComponentModel)this.GetService(typeof(SComponentModel));
+            _glyphService = model.GetService<IGlyphService>();
+
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                CommandID menuCommandID = new CommandID(GuidList.guidGlyphExporterCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
+                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
+            }
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.DesktopDirectory;
+                dialog.ShowDialog();
+
+                string folder = dialog.SelectedPath;
+
+                if (!string.IsNullOrEmpty(folder) && Directory.Exists(folder))
+                {
+                    SaveGlyphsToDisk(folder);
+                }
+            }
+        }
+
+        private void SaveGlyphsToDisk(string folder)
+        {
+            Array groups = Enum.GetValues(typeof(StandardGlyphGroup));
+            Array items = Enum.GetValues(typeof(StandardGlyphItem));
+
+            foreach (var groupName in groups)
+                foreach (var itemName in items)
+                {
+                    StandardGlyphGroup group = (StandardGlyphGroup)groupName;
+                    StandardGlyphItem item = (StandardGlyphItem)itemName;
+
+                    BitmapSource glyph = _glyphService.GetGlyph(group, item) as BitmapSource;
+
+                    if (glyph == null)
+                        continue;
+
+                    string fileName = Path.Combine(folder, group.ToString(), item.ToString() + ".png");
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+
+                    using (var fileStream = new FileStream(fileName, FileMode.Create))
+                    {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(glyph));
+                        encoder.Save(fileStream);
+                    }
+                }
+        }
+    }
+}
---
+++ GlyphExporter/Guids.cs
@@ -0,0 +1,19 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MadsKristensen.GlyphExporter
+{
+    static class GuidList
+    {
+        public const string guidGlyphExporterPkgString = "c1e1a7aa-d416-4600-81ce-a0aba0a3b201";
+        public const string guidGlyphExporterCmdSetString = "3fc922ee-7ea1-456e-86df-f5a5ad462cdc";
+
+        public static readonly Guid guidGlyphExporterCmdSet = new Guid(guidGlyphExporterCmdSetString);
+    }
+
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand = 0x100;
+    }
+}
