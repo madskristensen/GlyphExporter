4bf87be2bb1010ce511b33c73b64e3866fe0a252
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-07-29 14:23:57 -0700

    Fixed issue with high DPI displays

--- src/GlyphExporterPackage.cs
+++ src/GlyphExporterPackage.cs
@@ -19,17 +19,17 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace MadsKristensen.GlyphExporter
 {
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
-	[ProvideMenuResource("Menus.ctmenu", 1)]
-	[Guid(PackageGuids.guidGlyphExporterPkgString)]
-	public sealed class GlyphExporterPackage : AsyncPackage
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidGlyphExporterPkgString)]
+    public sealed class GlyphExporterPackage : AsyncPackage
     {
-		private IGlyphService _glyphService;
-		private IVsImageService2 _imageService;
+        private IGlyphService _glyphService;
+        private IVsImageService2 _imageService;
 
-		protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-		{
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
             await base.InitializeAsync(cancellationToken, progress);
 
             // When initialized asynchronously, we *may* be on a background thread at this point.
@@ -48,23 +48,25 @@ namespace MadsKristensen.GlyphExporter
             Assumes.Present(mcs);
 
             var cmdGlyphId = new CommandID(PackageGuids.guidGlyphExporterCmdSet, PackageIds.cmdidGlyph);
-			var menuGlyph = new MenuCommand(ButtonClicked, cmdGlyphId);
-			mcs.AddCommand(menuGlyph);
-		}
+            var menuGlyph = new MenuCommand(ButtonClicked, cmdGlyphId);
+            mcs.AddCommand(menuGlyph);
+        }
 
-		private void ButtonClicked(object sender, EventArgs e)
-		{
-			string folder = GetFolder();
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
 
-			if (!string.IsNullOrEmpty(folder) && Directory.Exists(folder))
-			{
-				SaveImagesToDisk(Path.Combine(folder, "images"));
-				SaveGlyphsToDisk(Path.Combine(folder, "glyphs"));
-			}
-		}
+            var folder = GetFolder();
 
-		private void SaveImagesToDisk(string folder)
-		{
+            if (!string.IsNullOrEmpty(folder) && Directory.Exists(folder))
+            {
+                SaveImagesToDisk(Path.Combine(folder, "images"));
+                SaveGlyphsToDisk(Path.Combine(folder, "glyphs"));
+            }
+        }
+
+        private void SaveImagesToDisk(string folder)
+        {
             ThreadHelper.ThrowIfNotOnUIThread();
 
             PropertyInfo[] monikers = typeof(KnownMonikers).GetProperties(BindingFlags.Static | BindingFlags.Public);
@@ -74,120 +76,124 @@ namespace MadsKristensen.GlyphExporter
                 Flags = (uint)_ImageAttributesFlags.IAF_RequiredFlags,
                 ImageType = (uint)_UIImageType.IT_Bitmap,
                 Format = (uint)_UIDataFormat.DF_WPF,
+                Dpi = 96,
                 LogicalHeight = 16,
                 LogicalWidth = 16,
                 StructSize = Marshal.SizeOf(typeof(ImageAttributes))
             };
 
             WriteableBitmap sprite = null;
-			int count = 0;
-			char letter = ' ';
+            var count = 0;
+            var letter = ' ';
 
-			foreach (PropertyInfo monikerName in monikers)
-			{
-				var moniker = (ImageMoniker)monikerName.GetValue(null, null);
-				IVsUIObject result = _imageService.GetImage(moniker, imageAttributes);
+            foreach (PropertyInfo monikerName in monikers)
+            {
+                var moniker = (ImageMoniker)monikerName.GetValue(null, null);
+                IVsUIObject result = _imageService.GetImage(moniker, imageAttributes);
 
-				if (monikerName.Name[0] != letter)
-				{
-					if (sprite != null)
-					{
-						sprite.Unlock();
-						SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", letter + ".png"));
-					}
+                if (monikerName.Name[0] != letter)
+                {
+                    if (sprite != null)
+                    {
+                        sprite.Unlock();
+                        SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", letter + ".png"));
+                    }
 
-					int items = monikers.Count(m => m.Name[0] == monikerName.Name[0]);
-					sprite = new WriteableBitmap(16, 16 * (items), 96, 96, PixelFormats.Pbgra32, null);
-					sprite.Lock();
-					letter = monikerName.Name[0];
-					count = 0;
-				}
+                    var items = monikers.Count(m => m.Name[0] == monikerName.Name[0]);
+                    sprite = new WriteableBitmap(16, 16 * (items), 96, 96, PixelFormats.Pbgra32, null);
+                    sprite.Lock();
+                    letter = monikerName.Name[0];
+                    count = 0;
+                }
 
-                result.get_Data(out object data);
+                result.get_Data(out var data);
 
                 if (data == null)
-					continue;
+                {
+                    continue;
+                }
 
-				var glyph = data as BitmapSource;
-				string fileName = Path.Combine(folder, monikerName.Name + ".png");
+                var glyph = data as BitmapSource;
+                var fileName = Path.Combine(folder, monikerName.Name + ".png");
 
-				int stride = glyph.PixelWidth * (glyph.Format.BitsPerPixel / 8);
-				byte[] buffer = new byte[stride * glyph.PixelHeight];
-				glyph.CopyPixels(buffer, stride, 0);
+                var stride = glyph.PixelWidth * (glyph.Format.BitsPerPixel / 8);
+                var buffer = new byte[stride * glyph.PixelHeight];
+                glyph.CopyPixels(buffer, stride, 0);
 
-				sprite.WritePixels(new Int32Rect(0, count, glyph.PixelWidth, glyph.PixelHeight), buffer, stride, 0);
-				count += 16;
+                sprite.WritePixels(new Int32Rect(0, count, glyph.PixelWidth, glyph.PixelHeight), buffer, stride, 0);
+                count += 16;
 
-				SaveBitmapToDisk(glyph, fileName);
-			}
+                SaveBitmapToDisk(glyph, fileName);
+            }
 
-			// Save the last image sprite to disk
-			sprite.Unlock();
-			SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", letter + ".png"));
-		}
+            // Save the last image sprite to disk
+            sprite.Unlock();
+            SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", letter + ".png"));
+        }
 
 
-		private void SaveGlyphsToDisk(string folder)
-		{
-			Array groups = Enum.GetValues(typeof(StandardGlyphGroup));
-			Array items = Enum.GetValues(typeof(StandardGlyphItem));
+        private void SaveGlyphsToDisk(string folder)
+        {
+            Array groups = Enum.GetValues(typeof(StandardGlyphGroup));
+            Array items = Enum.GetValues(typeof(StandardGlyphItem));
 
-			foreach (object groupName in groups)
-			{
-				int count = 0;
-				string glyphFolder = Path.Combine(folder, groupName.ToString());
-				var sprite = new WriteableBitmap(16, 16 * (items.Length), 96, 96, PixelFormats.Pbgra32, null);
-				sprite.Lock();
+            foreach (var groupName in groups)
+            {
+                var count = 0;
+                var sprite = new WriteableBitmap(16, 16 * (items.Length), 96, 96, PixelFormats.Pbgra32, null);
+                sprite.Lock();
 
-				foreach (object itemName in items)
-				{
-					var group = (StandardGlyphGroup)groupName;
-					var item = (StandardGlyphItem)itemName;
+                foreach (var itemName in items)
+                {
+                    var group = (StandardGlyphGroup)groupName;
+                    var item = (StandardGlyphItem)itemName;
 
                     if (!(_glyphService.GetGlyph(group, item) is BitmapSource glyph))
+                    {
                         continue;
+                    }
 
-                    string fileName = Path.Combine(folder, group.ToString(), item.ToString() + ".png");
+                    var fileName = Path.Combine(folder, group.ToString(), item.ToString() + ".png");
 
-					SaveBitmapToDisk(glyph, fileName);
+                    SaveBitmapToDisk(glyph, fileName);
 
-					int stride = glyph.PixelWidth * (glyph.Format.BitsPerPixel / 8);
-					byte[] data = new byte[stride * glyph.PixelHeight];
-					glyph.CopyPixels(data, stride, 0);
+                    var stride = glyph.PixelWidth * (glyph.Format.BitsPerPixel / 8);
+                    var data = new byte[stride * glyph.PixelHeight];
+                    glyph.CopyPixels(data, stride, 0);
 
-					sprite.WritePixels(
-						new Int32Rect(0, count, glyph.PixelWidth, glyph.PixelHeight),
-						data, stride, 0);
+                    sprite.WritePixels(
+                        new Int32Rect(0, count, glyph.PixelWidth, glyph.PixelHeight),
+                        data, stride, 0);
 
-					count += 16;
-				}
+                    count += 16;
+                }
 
-				sprite.Unlock();
-				SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", groupName + ".png"));
-			}
-		}
+                sprite.Unlock();
+                SaveBitmapToDisk(sprite, Path.Combine(folder, "_sprites", groupName + ".png"));
+            }
+        }
 
-		private static void SaveBitmapToDisk(BitmapSource glyph, string fileName)
-		{
-			Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+        private static void SaveBitmapToDisk(BitmapSource glyph, string fileName)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(fileName));
 
-			using (var fileStream = new FileStream(fileName, FileMode.Create))
-			{
-				BitmapEncoder encoder = new PngBitmapEncoder();
-				encoder.Frames.Add(BitmapFrame.Create(glyph));
-				encoder.Save(fileStream);
-			}
-		}
+            using (var fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(glyph));
+                encoder.Save(fileStream);
+            }
+        }
 
-		private static string GetFolder()
-		{
-			using (var dialog = new FolderBrowserDialog())
-			{
-				dialog.RootFolder = Environment.SpecialFolder.DesktopDirectory;
-				dialog.ShowDialog();
+        private static string GetFolder()
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.DesktopDirectory;
+                dialog.ShowDialog();
 
-				return dialog.SelectedPath;
-			}
-		}
-	}
+                return dialog.SelectedPath;
+            }
+        }
+    }
 }
